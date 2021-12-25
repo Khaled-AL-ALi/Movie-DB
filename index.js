6259b4e12cf488f6d3ee400310a86c108073a889
Khaled-AL-ALi <khaled2020pro@gmail.com>
2021-12-25 22:26:19 +0200

    step 4

--- index.js
+++ index.js
@@ -7,15 +7,26 @@ app.get('/', function (req, res) {
 });
 
 app.get('/test', function (req, res) {
-    res.send({status:200, message:"ok"});
+    res.send({ status: 200, message: "ok" });
 
 });
 const d = new Date();
 let hour = d.getHours();
 let minutes = d.getMinutes();
 app.get('/time', function (req, res) {
-    res.send({status:200, message:hour+':'+minutes});
+    res.send({ status: 200, message: hour + ':' + minutes });
 
+});
+
+app.get('/hello/:id', function (req, res) {
+    res.send({ status: 200, message: `hello ${req.params.id}` },);
+
+});
+
+app.get('/search', function (req, res) {
+    let { s } = req.query;
+    if (s) return res.send({ status: 200, message: "ok", data: s });
+    res.status(500).json({ status: 500, error: true, message: "you have to provide a search" });
 });
 
 var server = app.listen(3000, function () {
